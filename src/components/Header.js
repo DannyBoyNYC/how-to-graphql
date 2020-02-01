d225a68a993e11468c348c4dd01f99515d529fad
DannyBoyNYC <daniel.deverell@gmail.com>
2020-02-01 17:29:32 -0500

    done search

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
-import { AUTH_TOKEN } from './constants'
+import { AUTH_TOKEN } from './constants';
 
 class Header extends Component {
   render() {
-    const authToken = localStorage.getItem(AUTH_TOKEN)
+    const authToken = localStorage.getItem(AUTH_TOKEN);
     return (
       <div className="flex pa1 justify-between nowrap orange">
         <div className="flex flex-fixed black">
@@ -14,6 +14,12 @@ class Header extends Component {
           <Link to="/" className="ml1 no-underline black">
             new
           </Link>
+
+          <div className="ml1">|</div>
+          <Link to="/search" className="ml1 no-underline black">
+            search
+          </Link>
+
           {authToken && (
             <div className="flex">
               <div className="ml1">|</div>
@@ -28,8 +34,8 @@ class Header extends Component {
             <div
               className="ml1 pointer black"
               onClick={() => {
-                localStorage.removeItem(AUTH_TOKEN)
-                this.props.history.push(`/`)
+                localStorage.removeItem(AUTH_TOKEN);
+                this.props.history.push(`/`);
               }}
             >
               logout
@@ -41,7 +47,7 @@ class Header extends Component {
           )}
         </div>
       </div>
-    )
+    );
   }
 }
 
